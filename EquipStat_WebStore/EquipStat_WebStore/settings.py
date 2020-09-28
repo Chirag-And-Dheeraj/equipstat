2c3f53fafc73d3326ddacfda4bd71c00b3d12c78
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-09-28 23:17:09 +0530

    :bug:: fix secret key issue

--- EquipStat_WebStore/EquipStat_WebStore/settings.py
+++ EquipStat_WebStore/EquipStat_WebStore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets import SECRET_KEY
+from .secret import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

ba5127d51c7be1750c2cf62ccf0cda08d5a97670
Chirag Lulla <lullachirag239@gmail.coom>
2020-10-17 00:00:55 +0530

    :art:: modify navbar,contact form and improve format of code

--- EquipStat_WebStore/store/forms.py
+++ EquipStat_WebStore/store/forms.py
@@ -5,17 +5,17 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-
+classes = 'input_field placeholder-gray-700 focus:outline-none focus:shadow-myOutline'
 
 class ContactUsForm(ModelForm):
     class Meta:
         model = ContactUsDetail
         fields = '__all__'
-    firstName = forms.CharField(label="First Name " ,widget=forms.TextInput(attrs={'class':'w-full bg-gray-500 text-gray-900 placeholder-gray-700 p-3 rounded focus:outline-none focus:shadow-myOutline' , 'placeholder':'First Name'}))
-    lastName = forms.CharField(label="Last Name ", widget=forms.TextInput(attrs={'class':'w-full bg-gray-500 text-gray-900 placeholder-gray-700 p-3 rounded focus:outline-none focus:shadow-myOutline ','placeholder':'Last Name'}))
-    email = forms.EmailField(label="Email " ,widget=forms.TextInput(attrs={'class':'w-full bg-gray-500 text-gray-900 placeholder-gray-700 p-3 rounded focus:outline-none focus:shadow-myOutline ','placeholder':'Email Address'}))
-    contact = forms.CharField(label="Contact Number ",widget=forms.TextInput(attrs={'class':'w-full bg-gray-500 text-gray-900 placeholder-gray-700 p-3 rounded focus:outline-none focus:shadow-myOutline ','placeholder':'Contact'}))
+    firstName = forms.CharField(label="First Name " ,widget=forms.TextInput(attrs={'class': classes, 'placeholder':'John'}))
+    lastName = forms.CharField(label="Last Name ", widget=forms.TextInput(attrs={'class': classes,'placeholder':'Doe'}))
+    email = forms.EmailField(label="Email " ,widget=forms.TextInput(attrs={'class': classes,'placeholder':'example@domain.com'}))
+    contact = forms.CharField(label="Contact Number ",widget=forms.TextInput(attrs={'class': classes,'placeholder':'xxxxxxxxxx'}))
     typeOfMessage = forms.CharField(label="Type Of Message" ,widget=forms.RadioSelect(choices=MESSAGE_TYPES))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class':'w-full bg-gray-500 text-gray-900 placeholder-gray-700 p-3 rounded focus:outline-none focus:shadow-myOutline ' ,'placeholder':'Type Your Messasge here...', 'rows':'4' , 'cols':'20'}),label="Enquiry/Grievances")
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': classes ,'placeholder':'Type Your Messasge here...', 'rows':'4' , 'cols':'20'}),label="Enquiry/Grievances")
 
 

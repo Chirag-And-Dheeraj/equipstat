a88e72d1921bb236259402e411c43e827835a12a
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-04-08 04:25:48 +0530

    :passport_control: chore: add social login/signup backend

--- EquipStat_WebStore/settings.py
+++ EquipStat_WebStore/settings.py
@@ -39,8 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Project related apps
     'store',
+
+    # Social Auth apps
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
+
+SITE_ID = 2
+
+
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -83,6 +97,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)
@@ -136,3 +160,15 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
--- store/urls.py
+++ store/urls.py
@@ -5,22 +5,5 @@ from . import views
 urlpatterns = [
 	#Leave as empty string for base url
 	path('', views.home, name="home"),
-    path('register/', views.register, name="register"),
-    path('login/', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('user/<str:pk>', views.profile, name='profile'),
-    path('store/', views.store, name="store"),
-    path('store/<slug:slug>/', views.product, name="product"),
-    path('refurbished/', views.refurbished, name="refurbished"),
-    path('refurbished/<slug:slug>/', views.refurbishedProduct, name="refurbishedProduct"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
-	path('cart/', views.cart, name="cart"),
-	path('checkout/', views.checkout, name="checkout"),
-    path('place_order/', views.placeOrder, name="place_order"),
-    path('enlist/', views.enlist, name="enlist"),
-    path('update_item/', views.updateItem, name="update_item"),
-    path('delete_listing/', views.deleteListing, name="delete_listing"),
-    path('success/<str:pk>', views.success, name='success'), 
 ]
 

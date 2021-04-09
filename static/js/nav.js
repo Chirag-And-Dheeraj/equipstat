4b20b6b8a9f14623a55226a3cd8aa73feaddfcfa
Chirag Lulla <lullachirag239@gmail.coom>
2021-04-10 03:14:48 +0530

    :fire::lipstick:remove home page html and implement nav for mobile

---
+++ static/js/nav.js
@@ -0,0 +1,12 @@
+const hamburgur = document.querySelector('.hamburgur')
+const navItems = document.querySelector('.navItems')
+
+if(window.innerWidth < 640){
+    hamburgur.addEventListener('click', e => {
+        hamburgur.classList.toggle('change')
+        navItems.classList.toggle('hidden')
+    }) 
+}else {
+    hamburgur.remove()
+}
+

b33c5d9a4b90d7c5c0de0e3729c5171fb0f8691e
Chirag Lulla <lullachirag239@gmail.coom>
2021-04-25 04:04:35 +0530

    :card_file_box: chore[Backend]: add all image models to admin site

--- store/admin.py
+++ store/admin.py
@@ -9,8 +9,9 @@ admin.site.register(Product)
 admin.site.register(Book)
 admin.site.register(Instrument)
 admin.site.register(Labcoat)
+admin.site.register(BookImage)
+admin.site.register(LabcoatImage)
+admin.site.register(InstrumentImage)
 admin.site.register(ContactUsDetail)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-
-

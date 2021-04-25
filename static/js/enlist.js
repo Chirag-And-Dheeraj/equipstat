b8215e3e6152fbba6e338af1bad71119f7e1f994
Chirag Lulla <lullachirag239@gmail.coom>
2021-04-26 01:10:45 +0530

    :star: chore[Frontend]: Complete Enlist Page Co-authored-by: Dheeraj Lalwani <dheerajdlalwani@users.noreply.github.com>

--- static/js/enlist.js
+++ static/js/enlist.js
@@ -34,52 +34,96 @@ if(innerWidth < 768) {
 }
 // accordian animations - end - sm
 
-
-let file = null
-let image = document.querySelector('.includes-dd #image')
-const validFileExtensions = [...image.getAttribute('accept').split(',')]
-const uploadedImages = document.querySelector('.includes-dd .uploaded-images')
-if(innerWidth >= 768) {
-    //accordian chevron arrows back to normal
+//accordian chevron arrows back to normal
+if(innerWidth >= 768){
     chevronArrow.forEach(arrow => arrow.classList.add("transform", "-rotate-90"))
-
-    // drag and drop start
-    const dropArea = document.querySelector('.includes-dd .drag-area')
-
-    // when a file is dragging over drag-area
-    dropArea.addEventListener('dragover', e => {
-        e.preventDefault()
-        dropArea.children[0].children[0].classList.add('animate-bounce')
-    })
-
-    // when a file dragged over drag-area was left
-    dropArea.addEventListener('dragleave', e => {
-        dropArea.children[0].children[0].classList.remove('animate-bounce')
-    })
-
-    // when a file is dropped in drag-area
-    dropArea.addEventListener('drop', e => {
-        e.preventDefault()
-        dropArea.children[0].children[0].classList.remove('animate-bounce')
-        file = e.dataTransfer.files[0]
-        let list = new DataTransfer()
-        for(let file of e.dataTransfer.files){
-            list.items.add(file)
-        }
-        let myFileList = list.files
-        image.files = myFileList
-        console.log(image.files)
-        if(validFileExtensions.includes(file.type)){
-            fileReader.onload = () => {  //this event is fired when read from file is over
-                let fileUrl = fileReader.result
-                let imgTag = `<img src="${fileUrl}" width="50" height="50" alt="" title="">`
-                uploadedImages.innerHTML = imgTag
-                uploadedImages.classList.remove("hidden")
-                uploadedImages.classList.add("block")
-            }
-            fileReader.readAsDataURL(file) //asynchronous call to read data from file
-        }else{
-            console.log("This is Invalid as it is not an Image file", file)
-        }
-    })
 }
+
+
+let dragAndDrop = function() {
+    let image = document.querySelector('.includes-dd #image')
+
+    const validFileExtensions = [...image.getAttribute('accept').split(',')]
+
+    const browseFile = document.querySelector('.includes-dd #browse')
+
+    browseFile.addEventListener('click', () => {
+        image.click()
+    })
+
+
+    const uploadedImages = document.querySelector('.includes-dd .uploaded-images')
+
+    if(innerWidth >= 768) {
+        // drag and drop start
+        const dropArea = document.querySelector('.includes-dd .drag-area')
+
+        // when a file is dragging over drag-area
+        dropArea.addEventListener('dragover', e => {
+            e.preventDefault()
+            dropArea.children[0].children[0].classList.add('animate-bounce')
+            dropArea.classList.add("transition-all","border-pallete-buttonPrimary", "bg-gray-300")
+        })
+
+        // when a file dragged over drag-area was left
+        dropArea.addEventListener('dragleave', e => {
+            dropArea.children[0].children[0].classList.remove('animate-bounce')
+            dropArea.classList.remove("border-pallete-buttonPrimary", "bg-gray-300")
+        })
+
+        let noOfImagesUploaded = 0
+        // when a file is dropped in drag-area
+        dropArea.addEventListener('drop', e => {
+            e.preventDefault()
+            dropArea.classList.remove("border-pallete-buttonPrimary", "bg-gray-300")
+            dropArea.children[0].children[0].classList.remove('animate-bounce')
+
+            let list = new DataTransfer()
+            let discarded = false
+            if(noOfImagesUploaded >= 5){
+                alert("5 images are already uploaded.")
+            }
+            else{
+                for(let file of e.dataTransfer.files){
+                    if(noOfImagesUploaded <= 4){
+                        if(validFileExtensions.includes(file.type)){
+                            if(file.size <= 2097152){
+                                list.items.add(file)
+                                noOfImagesUploaded++
+                            }
+                            else
+                                discarded = true
+                        }else{
+                            discarded = true
+                        }
+                    }else{
+                        alert("Any five images will be taken")
+                        break
+                    }
+                }
+            }
+
+            
+
+            image.files = list.files
+            uploadedImages.classList.remove("hidden")
+            uploadedImages.classList.add("block")
+
+            for(let photo of image.files){
+                let fileReader = new FileReader()
+                fileReader.onload = () => {  //this event is fired when read from file is over
+                    let fileUrl = fileReader.result
+                    let imgTag = `<img class="inline mr-4" src="${fileUrl}" width="50" height="50" alt="${photo.name}" title="${photo.name}">`
+                    uploadedImages.innerHTML += imgTag
+                }
+                fileReader.readAsDataURL(photo)  //asynchronous call to read data from file
+            }
+
+            if(discarded){
+                alert("Some files were discarded, either beacause they exceeded the required size limit or they were not images")
+            }
+        })
+    }
+}
+
+dragAndDrop()

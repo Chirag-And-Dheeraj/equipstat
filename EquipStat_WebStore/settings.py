c4a073e1a8efeb8ce553f49241f8e0481fee2a59
Chirag Lulla <lullachirag239@gmail.coom>
2021-05-28 03:09:03 +0530

    :construction:create login signup template

--- EquipStat_WebStore/settings.py
+++ EquipStat_WebStore/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'EquipStat_WebStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

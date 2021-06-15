502a5c8b87d18710e13704bb1b213860ef852b6f
Chirag Lulla <lullachirag239@gmail.coom>
2021-06-15 22:50:27 +0530

    :construction:implement client side validation on forms, login form done

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,96 +1,35 @@
-function validateContact() {
-    var regName = /^[a-zA-Z]+$/;
-    var regEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    var regContact = /^[0-9]+$/;
-    var first_name = document.getElementById('id_firstName').value;
-    var last_name = document.getElementById('id_lastName').value;
-    var email = document.getElementById('id_email').value;
-    var contact = document.getElementById('id_contact').value;
-    if (!regName.test(first_name)) {
-        alert('Invalid first name');
-        document.getElementById('id_firstName').focus();
-        return false;
-    } else if (!regName.test(last_name)) {
-        alert('Invalid Last name');
-        document.getElementById('id_lastName').focus();
-        return false;
-    } else if (!regEmail.test(email)) {
-        alert('Invalid Email');
-        document.getElementById('id_email').focus();
-        return false;
-    } else if (!regContact.test(contact)) {
-        alert('Invalid Contact');
-        document.getElementById('id_contact').focus();
-        return false;
-    } else {
-        return true;
-    }
+const inputs = document.querySelectorAll('.validate')
+const submit = document.querySelector('input[type=submit]')
+
+submit.disabled = true
+
+const patterns = {
+    password: /^[\w@-]{8,}$/,
+    login: /^[a-z\d_]{3,}$/i, 
+    contact: /^\d{10}$/,
+    fname: /^[a-zA-z ?]{2,30}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 }
 
-function validateEnlist() {
-    var regName = /^[a-zA-Z0-9_ ]+$/;
-    var regReturn = /^[0-9]+$/;
-    var product_name = document.getElementById('id_name').value;
-    var expected_return = document.getElementById('id_expectedReturn').value;
-    if (!regName.test(product_name)) {
-        alert('Invalid product name');
-        document.getElementById('id_name').focus();
-        return false;
-    } else if (!regReturn.test(expected_return)) {
-        alert('Invalid input');
-        document.getElementById('id_expectedReturn').focus();
-        return false;
-    } else {
-        return true;
+let check = (inputField, regex) => { 
+    inputField.classList.remove('border-gray-300')
+    if(regex.test(inputField.value)){
+        inputField.classList.remove('border-red-500')
+        inputField.classList.add('border-green-500')
+    }else{
+        inputField.classList.remove('border-green-500')
+        inputField.classList.add('border-red-500')
     }
+
+    inputs.forEach(input => {
+        if(input.classList.contains('border-green-500')){
+            submit.disabled = false
+        }
+    }) 
 }
 
-
-function validateRegister() {
-    var regName = /^[a-zA-Z]+$/;
-    var regEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    var regContact = /^[0-9]+$/;
-    var regUsername = /^[a-zA-Z0-9_\-\.]+$/;
-    var regPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    var first_name = document.getElementById('first_name').value;
-    var last_name = document.getElementById('last_name').value;
-    var user_name = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var contact = document.getElementById('contact').value;
-    var password1 = document.getElementById('password1').value;
-    var password2 = document.getElementById('password2').value;
-
-    if (!regName.test(first_name)) {
-        alert('Invalid first name');
-        document.getElementById('first_name').focus();
-        return false;
-    } else if (!regName.test(last_name)) {
-        alert('Invalid Last name');
-        document.getElementById('last_name').focus();
-        return false;
-    } else if (!regEmail.test(email)) {
-        alert('Invalid Email');
-        document.getElementById('email').focus();
-        return false;
-    } else if (!regContact.test(contact)) {
-        alert('Invalid Contact');
-        document.getElementById('contact').focus();
-        return false;
-    } else if (!regUsername.test(user_name)) {
-        alert('Invalid Username');
-        document.getElementById('username').focus();
-        return false;
-    } else if (password1 != password2) {
-        alert('Passwords not same');
-        document.getElementById('password1').focus();
-        return false;
-    } else if (!regPassword.test(password1)) {
-        alert('Invalid Password');
-        document.getElementById('password1').focus();
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
+inputs.forEach(input => {
+    input.addEventListener('focusout', e => {
+      check(e.target, patterns[e.target.getAttribute('name')]) 
+    })
+}) 
